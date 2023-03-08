26355cb09946b03904da8c70a699f0b40357a9d9
Schneider <vijales2000@gmail.com>
2023-03-08 09:40:16 -0300

    fazendo migraçoes pendentes

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from escola.models import Alunos, Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cpf', 'rg', 'data_nacimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome')
+    list_per_page = (20)
--- escola/models.py
+++ escola/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class alunos(models.Model):
+class Alunos(models.Model):
     nome = models.CharField(max_length= 100, blank=False, null=False)
     cpf = models.CharField(max_length=11, blank=False, null=False)
     rg = models.CharField(max_length=11, blank=False, null=False)
@@ -9,7 +9,7 @@ class alunos(models.Model):
     def __str__(self):
         return self.nome
 
-class curso(models.Model):
+class Curso(models.Model):
     NIVEL = (
         ('B','Basico'),
         ('I','Intermediario'),
