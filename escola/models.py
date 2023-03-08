ac34304db9f7414af9a35c343e8d5ee4601ea998
Schneider <vijales2000@gmail.com>
2023-03-08 09:21:07 -0300

    costruindo models de escola

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class alunos(models.Model):
+    nome = models.CharField(max_length= 100, blank=False, null=False)
+    cpf = models.CharField(max_length=11, blank=False, null=False)
+    rg = models.CharField(max_length=11, blank=False, null=False)
+    data_nacimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class curso(models.Model):
+    NIVEL = (
+        ('B','Basico'),
+        ('I','Intermediario'),
+        ('A','Avançado'),
+    )
+    codigo = models.CharField(max_length= 10, blank=False, null=False)
+    descricao = models.CharField(max_length= 100, blank=False, null=False)
+    nivel = models.CharField(max_length= 1,choices=NIVEL, blank=False, null=False, default="B")
+
+    def __str__(self):
+        return self.descricao
+
