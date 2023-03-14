8e5ffbb7a3f77887bf0fa9afb35ed18b7b3992ae
Schneider <vijales2000@gmail.com>
2023-03-14 14:23:35 -0300

    alterando admin

--- escola/admin.py
+++ escola/admin.py
@@ -4,7 +4,7 @@ from escola.models import Aluno, Curso
 class Alunos(admin.ModelAdmin):
     list_display = ('id', 'nome', 'cpf', 'rg', 'data_nacimento')
     list_display_links = ('id', 'nome')
-    search_fields = ('nome')
+  #  search_fields = ('nome')
     list_per_page = (20)
 
 admin.site.register(Aluno, Alunos)
@@ -12,7 +12,7 @@ admin.site.register(Aluno, Alunos)
 class Cursos(admin.ModelAdmin):
     list_display = ('codigo', 'descricao', 'nivel')
     list_display_links = ('codigo', 'descricao')
-    search_fields = ('codigo')
+   # search_fields = ('codigo')
     list_per_page = (20)
 
 admin.site.register(Curso, Cursos)
