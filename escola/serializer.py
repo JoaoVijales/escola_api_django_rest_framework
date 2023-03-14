7c2d32c60d2d1c59d2bf1e04b3eac163d7e84a19
Schneider <vijales2000@gmail.com>
2023-03-14 14:28:31 -0300

    Atualizando as views para viewSets

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
 from escola.models import Curso, Aluno
 
+
 class AlunoSerializer(serializers.ModelSerializer):
-    class Meta:
+    
+    class Meta:     
         model: Aluno
         fields: '__all__'
 
+
+
 class CursoSerializer(serializers.ModelSerializer):
-    class Meta:
+    
+    class Meta:     
         model: Curso
         fields: '__all__'
         
