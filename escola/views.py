d3cc0552e5d6dcbbb1c8737580f6e6318e28b27b
Schneider <vijales2000@gmail.com>
2023-07-21 18:23:48 -0300

    criando viewset para alunos matriculados

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasSerializer
+from escola.serializer import ListaAlunosMatriculadosSerializer ,AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
 
 
 class AlunosViewSet(viewsets.ModelViewSet): 
@@ -34,4 +34,14 @@ class listaMatriculasAluno(generics.ListAPIView):
     def get_queryset(self):
         queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
         return queryset
-    serializer_class = ListaMatriculasSerializer
+    serializer_class = ListaMatriculasAlunoSerializer
+
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando as alunos e alunas matriculados em um curso"""
+    
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+    
